bcf35eadcf484049ec36e7c2b510b91bbb43035f
Shivraj <sdkadgond9+github@gmail.com>
2025-01-14 11:16:38 +0530

    vercel

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import { inject } from "@vercel/analytics/*";
+import { inject } from "@vercel/analytics";
 
 inject();
 
