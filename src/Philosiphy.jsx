4b15271e4588d0fd63ed707a7eb0162ad09acd17
Shivraj <sdkadgond9+github@gmail.com>
2025-01-14 17:47:52 +0530

    changes fix

--- src/Philosiphy.jsx
+++ src/Philosiphy.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 
 const Philosiphy = ({ language, setLanguage }) => {
   const contentData = [
@@ -27,10 +27,10 @@ const Philosiphy = ({ language, setLanguage }) => {
           <p style={{ wordBreak: "keep-all" }}>
             Our individual investment plans are bespoke on every level. Through
             detailed health checks, we explore the depth and breadth of each
-            client’s unique portfolio – to see where risks lie and possibility
+            client's unique portfolio – to see where risks lie and possibility
             awaits. We champion clients to reach informed decisions about the
             opportunities and risks that stand before them, carefully crafting
-            customised financial strategies that suit the future they’re looking
+            customised financial strategies that suit the future they're looking
             to build.
           </p>,
         ],
@@ -53,13 +53,13 @@ const Philosiphy = ({ language, setLanguage }) => {
       english: {
         title: "We See the Value in Understanding Risk",
         description:
-          "Growth is only sustainable when it’s informed. We take a close look at each client’s portfolio to identify where risk is hiding beneath the surface. We believe that only when we properly understand risk today, can we build enduring growth for our clients tomorrow.",
+          "Growth is only sustainable when it's informed. We take a close look at each client's portfolio to identify where risk is hiding beneath the surface. We believe that only when we properly understand risk today, can we build enduring growth for our clients tomorrow.",
       },
       korean: {
         title: "철저한 리스크에 대한 이해, 성장의 첫 걸음",
         description: [
           <p style={{ wordBreak: "keep-all" }}>
-            Growth is only sustainable when it’s informed. 진정한 성장은
+            Growth is only sustainable when it's informed. 진정한 성장은
             리스크를 제대로 파악하는 것에서 시작됩니다. 고객의 포트폴리오를
             면밀히 분석해 겉으로 드러나지 않은 위험 요소를 찾아내고 관리합니다.
             오늘의 리스크를 깊이 있게 분석하고 이해해야만 안정적이고 지속 가능한
@@ -72,7 +72,7 @@ const Philosiphy = ({ language, setLanguage }) => {
       english: {
         title: "Our Independence is Our Integrity",
         description:
-          "We serve our clients and our clients only. As an independent business, untethered from the interests of large financial groups, every decision we make intelligently enhances each client’s portfolios. We are deft in our approach to offer the best guidance for each client’s wealth management – ensuring transparency, privacy, and security.",
+          "We serve our clients and our clients only. As an independent business, untethered from the interests of large financial groups, every decision we make intelligently enhances each client's portfolios. We are deft in our approach to offer the best guidance for each client's wealth management – ensuring transparency, privacy, and security.",
       },
       korean: {
         title: "독립성과 진정성",
@@ -91,7 +91,7 @@ const Philosiphy = ({ language, setLanguage }) => {
 
   return (
     <section className="bg-[#e7e6e2] py-16 px-5 md:px-10" id="philosophy">
-      <p className="text-[#545454] text-3xl py-10 lg:-ml-5 "> OUR PHILOSOPHY</p>
+      <p className="text-[#545454] text-3xl py-10 lg:-ml-5"> OUR PHILOSOPHY</p>
       {language ? (
         <div className="flex flex-col lg:flex-row lg:items-start lg:gap-10">
           <div className="lg:w-[50%] lg:mr-10 lg:-ml-5 md:text-justify">
@@ -109,32 +109,34 @@ const Philosiphy = ({ language, setLanguage }) => {
             ))}
           </div>
           {/* Video Section */}
-          <div className="lg:w-[50%] ml-auto lg:-mr-4 lg:mt-1">
-            <div className="lg:h-[570px] md:h-[652px] h-[400px]  overflow-hidden">
-              <video
-                className="w-full h-full object-cover"
-                autoPlay
-                loop
-                muted
-                playsInline
-                src="LV3N.mp4"
-              >
-                Your browser does not support the video tag.
-              </video>
-            </div>
-            <div>
-              <p className="mt-1 text-[#545454] font-light flex-[3] text-sm md:text-xl lg:text-base opacity-90">
-                Growth is only sustainable when it's informed. We take a close
-                look at each client's portfolio to identify where risk is hiding
-                beneath the surface. We believe that only when we properly
-                understand risk today.
-              </p>
+          <div className="lg:w-[50%] ml-auto lg:-mr-4">
+            <div className="lg:sticky lg:top-16 pt-[0.75rem]">
+              <div className="h-[400px] sm:h-[500px] md:h-[600px] lg:h-[750px] 2xl:h-[900px] relative">
+                <video
+                  className="w-full h-full object-cover"
+                  autoPlay
+                  loop
+                  muted
+                  playsInline
+                  src="LV3N.mp4"
+                >
+                  Your browser does not support the video tag.
+                </video>
+              </div>
+              <div className="mt-4">
+                <p className="text-[#545454] font-light text-sm md:text-xl lg:text-base opacity-90">
+                  Growth is only sustainable when it's informed. We take a close
+                  look at each client's portfolio to identify where risk is
+                  hiding beneath the surface. We believe that only when we
+                  properly understand risk today.
+                </p>
+              </div>
             </div>
           </div>
         </div>
       ) : (
-        <div className="flex flex-col lg:flex-row lg:gap-10">
-          <div className="lg:w-[50%] lg:mr-10 lg:-ml-5 md:text-justify gap-4 md:gap-8">
+        <div className="flex flex-col lg:flex-row lg:items-start lg:gap-10">
+          <div className="lg:w-[50%] lg:mr-10 lg:-ml-5 md:text-justify">
             {contentData.map((item, index) => (
               <React.Fragment key={index}>
                 <div className="text-[#545454] space-y-2 md:space-y-4 mb-16">
@@ -152,29 +154,31 @@ const Philosiphy = ({ language, setLanguage }) => {
             ))}
           </div>
           {/* Video Section */}
-          <div className="lg:w-[50%] ml-auto lg:-mr-4 lg:mt-1">
-            <div className="lg:max-h-[750px] md:h-[725px] h-[400px] overflow-hidden">
-              <video
-                className="w-full h-full  object-cover"
-                autoPlay
-                loop
-                muted
-                playsInline
-                src="LV3N.mp4"
-              >
-                Your browser does not support the video tag.
-              </video>
-            </div>
-            <div>
-              <p
-                className="mt-1 text-[#545454] font-light flex-[3] text-sm md:text-xl lg:text-base opacity-90"
-                style={{ wordBreak: "keep-all" }}
-              >
-                성장은 정보에 기반할 때만 지속 가능하다고 믿습니다. 우리는 각
-                클라이언트의 포트폴리오를 면밀히 살펴보아 위험이 어디에 숨겨져
-                있는지 파악합니다. 오늘날의 위험을 제대로 이해할 때만 지속
-                가능한 성장이 이루어진다고 생각합니다.
-              </p>
+          <div className="lg:w-[50%] ml-auto lg:-mr-4">
+            <div className="lg:sticky lg:top-16 pt-[1.75rem]">
+              <div className="h-[400px] sm:h-[500px] md:h-[600px] lg:h-[750px] 2xl:h-[600px] relative">
+                <video
+                  className="w-full h-full object-cover"
+                  autoPlay
+                  loop
+                  muted
+                  playsInline
+                  src="LV3N.mp4"
+                >
+                  Your browser does not support the video tag.
+                </video>
+              </div>
+              <div className="mt-4">
+                <p
+                  className="text-[#545454] font-light text-sm md:text-xl lg:text-base opacity-90"
+                  style={{ wordBreak: "keep-all" }}
+                >
+                  성장은 정보에 기반할 때만 지속 가능하다고 믿습니다. 우리는 각
+                  클라이언트의 포트폴리오를 면밀히 살펴보아 위험이 어디에 숨겨져
+                  있는지 파악합니다. 오늘날의 위험을 제대로 이해할 때만 지속
+                  가능한 성장이 이루어진다고 생각합니다.
+                </p>
+              </div>
             </div>
           </div>
         </div>
