4c80d6e4433100c55e2e23354e6706af337ba9c8
Shivraj <sdkadgond9+github@gmail.com>
2025-01-16 22:44:56 +0530

    fix

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,5 +1,5 @@
 const Content = ({ language }) => (
-  <div className="absolute bottom-0 right-0 md:right-[-18px] left-0 md:left-auto md:max-w-3xl lg:max-w-4xl p-2 md:p-12 lg:p-4 text-white z-20">
+  <div className="absolute bottom-0  md:bottom-[16px] right-0 md:right-[-18px] left-0 md:left-auto md:max-w-3xl lg:max-w-4xl p-2 md:p-12 lg:p-4 text-white z-20">
     <div className="relative">
       <div className="text-base font-light !pl-3 md:pl-0">
         {language ? (
