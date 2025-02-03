d65fdf2ec350e3a2ce4d40a1156ef4c817eb56b2
Shivraj <sdkadgond9+github@gmail.com>
2025-02-03 14:24:39 +0530

    new hero video

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -366,7 +366,7 @@ const LandingPage = ({ language, setLanguage, loading, setLoading }) => {
           <video
             ref={videoRef}
             className="absolute inset-0 w-full h-full object-cover scale-110"
-            src="hero.mp4"
+            src="heronew.mp4"
             autoPlay
             playsInline
             loop
